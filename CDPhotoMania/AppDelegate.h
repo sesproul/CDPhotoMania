e9844afdc15bf44b938f44a06f7110a3f1d21440
Stephen Sproul <sesproul@gmail.com>
2013-06-07 21:46:05 -0400

    Initial Commit

---
+++ CDPhotoMania/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CDPhotoMania
+//
+//  Created by sesproul on 6/7/13.
+//  Copyright (c) 2013 SDS.website.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CDPhotoMania/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CDPhotoMania
+//
+//  Created by sesproul on 6/7/13.
+//  Copyright (c) 2013 SDS.website.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
